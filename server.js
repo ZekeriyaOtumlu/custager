3f741f5e41c2058f120aa4125a0ef8346bea448b
Zack <zotumlu78@gmail.com>
2020-02-20 19:07:46 -0500

    adding files

---
+++ server.js
@@ -0,0 +1,30 @@
+const routes = require("./routes");
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// Define Middleware here
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// For heroku
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+}
+
+// Define API routes here
+app.use(routes)
+
+//Connect to mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks")
+
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
+
+
