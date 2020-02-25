60166c486b690fad35f52ba917c0b979038e3301
Zack <zotumlu78@gmail.com>
2020-02-25 18:45:48 -0500

    Saved Books updated

--- models/book.js
+++ models/book.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = Schema ({
+const bookSchema = new Schema ({
     title: {type: String, required: true},
     authors: [{type: String, required: true}],
     description: String,
